2b9b6bd903210fe1886c451d1336339d8e39c456
MaitreGobz <nayetlucas@hotmail.fr>
2026-01-29 17:24:53 +0100

    config of certification done

--- back-end/src/Service/Private/LessonProgressService.php
+++ back-end/src/Service/Private/LessonProgressService.php
@@ -6,9 +6,12 @@ use App\Entity\CursusValidation;
 use App\Entity\Lesson;
 use App\Entity\LessonValidation;
 use App\Entity\User;
+use App\Entity\Theme;
+use App\Entity\Certification;
 use App\Repository\LessonRepository;
 use App\Repository\LessonValidationRepository;
 use App\Repository\CursusValidationRepository;
+use App\Repository\CertificationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 /**
@@ -21,6 +24,7 @@ final class LessonProgressService
         private readonly LessonRepository $lessonRepository,
         private readonly LessonValidationRepository $lessonValidationRepository,
         private readonly CursusValidationRepository $cursusValidationRepository,
+        private readonly CertificationRepository $certificationRepository,
     ) {}
 
     public function validateLesson(User $user, Lesson $lesson): array
@@ -46,12 +50,12 @@ final class LessonProgressService
 
         // Check if the entire cursus is validated
         $cursus = $lesson->getCursus();
-        $totalLessons = $this->lessonRepository->count(['cursus' => $cursus]);
+        $totalLessons = $this->lessonRepository->countActiveByCursusId($cursus->getId());
         $validatedCount = $this->lessonValidationRepository->validatedLessonInCursusByUser($user, $cursus->getId());
 
         $cursusValidated = false;
 
-        if ($totalLessons > 0 && $validatedCount >= $totalLessons) {
+        if ($totalLessons > 0 && $validatedCount === $totalLessons) {
             $cvExisting = $this->cursusValidationRepository->findOneBy([
                 'user' => $user,
                 'cursus' => $cursus,
@@ -67,6 +71,9 @@ final class LessonProgressService
                 $this->em->flush();
 
                 $cursusValidated = true;
+
+                $theme = $cursus->getTheme();
+                $this->createThemeCertification($user, $theme);
             }
         }
 
@@ -75,5 +82,52 @@ final class LessonProgressService
             'cursusValidated' => $cursusValidated,
             'cursusId' => $cursus->getId(),
         ];
+    }
+
+    /**
+     * Create a certification for the user
+     */
+    private function createThemeCertification(User $user, ?Theme $theme): bool
+    {
+        // If theme is null, cannot create certification
+        if ($theme === null) {
+            return false;
+        }
+
+        // Check if certification already exists
+        $existingCert = $this->certificationRepository->findOneBy([
+            'user' => $user,
+            'theme' => $theme,
+        ]);
+
+        // If certification exists, do not create a new one
+        if ($existingCert) {
+            return false;
+        }
+
+        // Check if all lessons in the theme are validated
+        $totalLessonsInTheme = $this->lessonRepository->countActiveByThemeId($theme->getId());
+        if ($totalLessonsInTheme <= 0) {
+            return false;
+        }
+
+        // Count validated lessons in the theme by the user
+        $validatedLessonInTheme = $this->lessonValidationRepository->validatedLessonsInThemeByUser($user, $theme->getId());
+
+        // If not all lessons are validated, do not create certification
+        if ($validatedLessonInTheme !== $totalLessonsInTheme) {
+            return false;
+        }
+
+        // Create new Certification
+        $cert = new Certification();
+        $cert->setUser($user);
+        $cert->setTheme($theme);
+        $cert->setValidatedAt(new \DateTime());
+
+        $this->em->persist($cert);
+        $this->em->flush();
+
+        return true;
     }
 }
